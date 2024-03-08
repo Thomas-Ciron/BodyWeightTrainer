c8ce38b93a4d184159156945b014dca78e52bdcd
Thomas-Ciron <thomas.ciron@outlook.com>
2024-03-08 18:39:55 +0100

    glog: compile methods

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # BodyWeightTrainer
+Goal : giving user's data, build an optimized training session to let it muscle what it wants to train. This package will be integrated to a mobile app.
+
+## How to use SportTrainingSession ?
+### Requirements
+#### Install kotlin
+### How to compile ?
+On Linux : 
+```sh
+./compile.sh
+```
---
+++ compile.sh
@@ -0,0 +1,14 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo "Compiling Kotlin file..."
+kotlinc main.kt -include-runtime -d output.jar
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Compilation failed.${NC}" >&2
+    exit 1
+fi
+echo "Compilation successful."
+echo "Running output..."
+java -jar output.jar
+echo -e "${GREEN}Execution complete.${NC}"
