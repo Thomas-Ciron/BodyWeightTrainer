48e8b429b772ce1ebc00e5c5c365a94563bdc81a
Thomas-Ciron <thomas.ciron@outlook.com>
2024-03-08 19:15:19 +0100

    fix: update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,14 +1,15 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+output="BodyWeightTrainer.jar"
 
 echo "Compiling Kotlin files..."
-kotlinc src/*.kt -include-runtime -d BodyWeightTrainer.jar
+kotlinc src/*.kt -include-runtime -d $output
 if [ $? -ne 0 ]; then
     echo -e "${RED}Compilation failed.${NC}" >&2
     exit 1
 fi
 echo "Compilation successful."
 echo "Running output..."
-java -jar output.jar
+java -jar $output
 echo -e "${GREEN}Execution complete.${NC}"
--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,9 @@
+fun getExercisesEfficiency():List<List<Double>> {
+    return getCsvFloatContent("exercises_efficiency.csv")
+}
+
 fun main() {
     val coeffs = getFileFloatLines("coeffs.txt")
-    println(coeffs)
+    val trainedMusclesPerExo = getExercisesEfficiency().toMutableList()
+    println(trainedMusclesPerExo)
 }
