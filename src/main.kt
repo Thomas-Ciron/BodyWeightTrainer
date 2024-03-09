8f438238f3d156b67c608ecb41fd7253453bd797
Thomas-Ciron <thomas.ciron@outlook.com>
2024-03-09 12:02:28 +0100

    feat: current sport level added

--- src/main.kt
+++ src/main.kt
@@ -2,9 +2,15 @@ fun getExercisesEfficiency():List<List<Double>> {
     return getCsvFloatContent("exercises_efficiency.csv")
 }
 
+fun getCurrentLevel():Int {
+    return getIntFileContent("niveau de sport.txt")
+}
+
 fun main() {
     val coeffs = getFileFloatLines("coeffs.txt")
     val trainedMusclesPerExo = getExercisesEfficiency().toMutableList()
     val exercises = getFileLines("exercices.txt")
-    println(exercises)
+    var result = MutableList(exercises.size) {0}
+    result[1] = getCurrentLevel()
+    println(result)
 }
